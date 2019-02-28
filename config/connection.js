c12ff7754028d01ff49fbb801655a19ffe0d2407
Danielle Dees <d.danielle83@gmail.com>
2019-02-27 23:51:05 -0500

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,21 @@
 //set up sql connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
-  
     // Your port; if not 3306
     port: 3306,
-  
     // Your username
     user: "root",
-  
     // Your password
     password: "rootpassword",
     database: "burgers_db"
   });
+}
 
   //make connection
   connection.connect(function(err) {

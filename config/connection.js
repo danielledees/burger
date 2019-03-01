6140bd9f221dd6fdf72ea772b9c6cd07a7833df3
Danielle Dees <d.danielle83@gmail.com>
2019-02-28 19:58:08 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,6 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    // Your port; if not 3306
-    port: 3306,
     // Your username
     user: "root",
     // Your password
@@ -18,13 +16,15 @@ if (process.env.JAWSDB_URL) {
 }
 
   //make connection
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+  connection.connect();
+  
+  // (function(err) {
+  //   if (err) {
+  //     console.error("error connecting: " + err.stack);
+  //     return;
+  //   }
+  //   console.log("connected as id " + connection.threadId);
+  // });
   
   // Export connection to ORM 
   module.exports = connection;
